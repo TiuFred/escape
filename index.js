4bb6fe0feefac7e90780238f7d14c9d2ced85462
TiuFred <145013978+TiuFred@users.noreply.github.com>
2025-04-07 14:25:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+// index.js
+const express = require('express');
+const path = require('path');
+const app = express();
 
+// Serve os arquivos estáticos da raiz do projeto
+app.use(express.static(path.join(__dirname, '/')));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
